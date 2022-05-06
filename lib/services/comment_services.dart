3a19a4fd9653f21b0898a9780169cebbecab9743
rezakurniasetiawan <56224328+rezakurniasetiawan@users.noreply.github.com>
2022-05-06 20:50:59 +0700

    Fitur Comment

---
+++ lib/services/comment_services.dart
@@ -0,0 +1,144 @@
+import 'dart:convert';
+
+import 'package:besty_apps/model/api_response_model.dart';
+import 'package:besty_apps/model/comment_mode.dart';
+import 'package:besty_apps/services/auth_services.dart';
+import 'package:http/http.dart' as http;
+
+import '../constan.dart';
+
+String postsURL = 'https://besttyapp.skom.id/api/posts';
+// Get post comments
+Future<ApiResponse> getComments(int postId) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    final response = await http.get(Uri.parse('$postsURL/$postId/comments'),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        });
+
+    switch (response.statusCode) {
+      case 200:
+        // map each comments to comment model
+        apiResponse.data = jsonDecode(response.body)['comments']
+            .map((p) => Comment.fromJson(p))
+            .toList();
+        apiResponse.data as List<dynamic>;
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWhentWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+// Create comment
+Future<ApiResponse> createComment(int postId, String? comment) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    final response = await http.post(Uri.parse('$postsURL/$postId/comments'),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+        body: {
+          'comment': comment
+        });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = jsonDecode(response.body);
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWhentWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+String commentsURL = 'https://besttyapp.skom.id/api/comments';
+// Delete comment
+Future<ApiResponse> deleteComment(int commentId) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    final response = await http.delete(Uri.parse('$commentsURL/$commentId'),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = jsonDecode(response.body)['message'];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWhentWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+// Edit comment
+Future<ApiResponse> editComment(int commentId, String comment) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    final response = await http.put(Uri.parse('$commentsURL/$commentId'),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+        body: {
+          'comment': comment
+        });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = jsonDecode(response.body)['message'];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWhentWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
